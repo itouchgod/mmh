a67c58d10998bb002eea60d1c29ee4dd3548bfa8
Roger Luo <ukluocn@gmail.com>
2026-05-26 22:49:19 +0800

    Initial Commit

---
+++ MMH/MMHApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMHApp.swift
+//  MMH
+//
+//  Created by Roger on 2026-05-26.
+//
+
+import SwiftUI
+
+@main
+struct MMHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
